79cab729358d307b98643dbf312937e7e7516fab
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-06-18 18:09:12 -0500

    generate array of random integers and validate against password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,26 @@ let includeUppercaseLetters = false;
 let includeNumbers = false;
 let includeSpecialCharacters = false;
 
-// Function to prompt user to enter password criteria
-let passwordCriteria = function() {
+let passwordArray = [];
 
+const basicLatinLower = 33;
+const unicodeZero = 48;
+const unicodeNine = 57;
+const unicodeUppercaseA = 65;
+const unicodeUppercaseZ = 90;
+const unicodeLowercaseA = 97;
+const unicodeLowercaseZ = 122;
+const basicLatinUpper = 126;
+
+// Function to prompt user to enter valid password criteria
+let passwordCriteria = function() {
   // Prompt user for password length and validate input
   while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = parseInt(prompt(`Please enter a password length between 8 and 128 characters:`));
     if (isNaN(passwordLength)) {
       passwordLength = 0;
     }
-  };
+  }
 
   // Prompt user regarding inclusion of various character types in password - at least one character type must be selected
   while (includeLowercaseLetters === false && includeUppercaseLetters === false && includeNumbers === false && includeSpecialCharacters === false) {
@@ -27,30 +37,86 @@ let passwordCriteria = function() {
     console.log(passwordLength, includeLowercaseLetters, includeUppercaseLetters, includeNumbers, includeSpecialCharacters);
   }
 }
-// *** WHERE DO I PUT passwordCriteria() FUNCTION CALL? ***
 
-// Generate random password that meets criteria
-let generatePassword = function() {
-  // Arguments
-    // length (integer)
-    // includeLowercaseLetters (boolean)
-    // includeUppercaseLetters (boolean)
-    // includeNumbers (boolean)
-    // includeSymbols (boolean)
+// Function to generate a random integer between a minimum and maximum value
+let randomNumber = function(min, max) {
+  let value = Math.floor(Math.random() * (max - min + 1) + min);
 
-  // While all character criteria are not met in generated array, generate array
+  return value;
+}
+
+// Function to create an array of random integers (Unicode values) that correspond to Basic Latin characters
+let createPasswordArray = function(length) {
+  for (i = 0; i < passwordLength; i++) {
+    passwordArray[i] = randomNumber(basicLatinLower, basicLatinUpper);
+  }
+}
+
+// Function to validate array created against password criteria
+let validatePasswordArray = function(array, lowercase, uppercase, number, special) {
+  // If password must contain lowercase letters, then...
+  if (lowercase) {
+    // Function to check array for integers (Unicode values) that correspond to lowercase letters
+    let checkArrayLowercase = function(lowercase) {
+      return lowercase >= unicodeLowercaseA && lowercase <= unicodeLowercaseZ;
+    }
     
-    // For i to length entered by user, generate an array of random integers 33 - 126 (Unicode values for Basic Latin characters)
+    if (isNaN(array.find(checkArrayLowercase))) {
+      // Array does not contain any lowercase letters, so create a new password array
+      createPasswordArray(passwordLength);
+    }
+  }
 
-    // Validate array against password criteria
-      // IF includeLowercaseLetters = 'true' THEN array must contain at least one integer between 97 and 122
-      // IF includeUppercaseLetters = 'true' THEN array must contain at least one integer between 65 and 90
-      // IF includeNumbers = 'true' THEN array must contain at least one integer between 48 and 57
-      // IF includeSymbols = 'true' THEN array must contain at least one integer
-        // between 33 and 47 OR
-        // between 58 and 64 OR
-        // between 91 and 96 OR
-        // between 123 and 126
+  // If password must contain uppercase letters, then...
+  if (uppercase) {
+    // Function to check array for integers (Unicode values) that correspond to uppercase letters
+    let checkArrayUppercase = function(uppercase) {
+      return uppercase >= unicodeUppercaseA && uppercase <= unicodeUppercaseZ;
+    }
+
+    if (isNaN(array.find(checkArrayUppercase))) {
+      // Array does not contain any uppercase letters, so create new password array
+      createPasswordArray(passwordLength);
+    }
+  }
+
+  // If password must contain numbers, then...
+  if (number) {
+    // Function to check array for integers (Unicode values) that correspond to numbers
+    let checkArrayNumber = function(number) {
+      return number >= unicodeZero && number <= unicodeNine;
+    }
+
+    if (isNaN(array.find(checkArrayNumber))) {
+      // Array does not contain any numbers, so create new password array
+      createPasswordArray(passwordLength);
+    }
+  }
+
+  // If password must contain special characters, then...
+  if (special) {
+    // Function to check array for integers (Unicode values) that correspond to special characters
+    let checkArraySpecial = function(special) {
+      return (special >= 33 && special <= 47) || (special >= 58 && special <= 64) || (special >= 91 && special <= 96) || (special >= 123 && special <= 126);
+    }
+
+    if (isNaN(array.find(checkArraySpecial))) {
+      // Array does not contain any special characters, so create new password array
+      createPasswordArray(passwordLength);
+    }
+  }
+}
+
+// Function to generate random password that meets criteria
+let generatePassword = function() {
+  // Prompt user to enter valid password criteria
+  passwordCriteria();  
+
+  // Create an array of random integers (Unicode values) that correspond to Basic Latin characters
+  createPasswordArray(passwordLength);
+
+  // validate array created against password criteria
+  validatePasswordArray(passwordArray, includeLowercaseLetters, includeUppercaseLetters, includeNumbers, includeSpecialCharacters);
 
   // Convert each element in array from Unicode value (integer) to character
 
